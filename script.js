a4a46fff3481bbdd66afa4d10f9faad18ba6ce40
lewinther <125894035+lewinther@users.noreply.github.com>
2023-03-10 10:15:37 +0100

    No head/prod overlap + comment out Carousel func

--- script.js
+++ script.js
@@ -143,19 +143,19 @@ function getProductInfo() {
   document.querySelector(".product-img").setAttribute("src", productImgSrc);
 }
 
-$('.carousel .carousel-item').each(function () {
-  var minPerSlide = 1;
-  var next = $(this).next();
-  if (!next.length) {
-  next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+// $('.carousel .carousel-item').each(function () {
+//   var minPerSlide = 1;
+//   var next = $(this).next();
+//   if (!next.length) {
+//   next = $(this).siblings(':first');
+//   }
+//   next.children(':first-child').clone().appendTo($(this));
   
-  for (var i = 0; i < minPerSlide; i++) {
-    next=next.next(); 
-    if (!next.length) { 
-      next=$(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this)); 
-  } 
-});
+//   for (var i = 0; i < minPerSlide; i++) {
+//     next=next.next(); 
+//     if (!next.length) { 
+//       next=$(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this)); 
+//   } 
+// });
